e482fd90f1c0305fe553a912f021db3be5c64ece
sourav <souravdas360@gmail.com>
2023-07-05 00:45:36 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+//1. Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware!');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+/*
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!...', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this URL!...');
+});
+
+*/
+
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getSpecificTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+//3. Routes
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
---
+++ routes/tourRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const tourController = require('./../controllers/tourController');
+const router = express.Router();
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+router
+  .route('/:id')
+  .get(tourController.getSpecificTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+module.exports = router;
